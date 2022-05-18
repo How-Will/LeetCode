6ff19a3bae784df3c683e9b87f56f88583a25393
william.Ubuntu <huo843435402@gmail.com>
2022-05-18 14:25:03 +0800

    unified programming style

--- 704_binary_search.cpp
+++ 704_binary_search.cpp
@@ -1,39 +1,41 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-    int search(vector<int>& nums, int target){
-        int lower_index = 0;
-        int higher_index = nums.size() - 1; // 初始搜索范围[lower_index, higher_index]
-        
-        while(lower_index <= higher_index){
-            int median_index = (lower_index + higher_index) / 2;
-            if(nums[median_index] > target){
-                // 调整搜索范围[lower_index, median_index - 1]
-                higher_index = median_index - 1;
-            }else if(nums[median_index] < target){
-                // 调整搜索范围[median_index + 1, higher_index]
-                lower_index = median_index + 1;
-            }else{
-                // 找到了target，返回下标
-                return median_index;
-            }
-        }
+  int search(vector<int> &nums, int target) {
+    int left = 0;
+    int right = nums.size() - 1; // 初始搜索范围[left, right]
 
-        return -1;  // 数组中不存在target
+    while (left <= right) {
+      int middle = (left + right) / 2;
+      if (nums[middle] < target) {
+        // middle左侧的元素都小于target
+        // 调整搜索范围[middle + 1, right]
+        left = middle + 1;
+      } else if (nums[middle] > target) {
+        // middle右侧的元素都大于target
+        // 调整搜索范围[left, middle - 1]
+        right = middle - 1;
+      } else {
+        // 找到了target，返回下标
+        return middle;
+      }
     }
+
+    return -1; // 数组中不存在target
+  }
 };
 
-int main(){
-    vector<int> ivec{-1, 0, 3, 5, 9, 12};
-    int target = 9;
-    Solution* sol = new Solution();
+int main() {
+  vector<int> ivec{-1, 0, 3, 5, 9, 12};
+  int target = 9;
+  Solution *sol = new Solution();
 
-    int res = sol->search(ivec, target);
+  int res = sol->search(ivec, target);
 
-    cout << "res = " << res << endl;
+  cout << "res = " << res << endl;
 
-    return 0;
+  return 0;
 }
