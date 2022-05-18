6472e1259469af9fbc25040c982eec4d7d0cb7b9
william.Ubuntu <huo843435402@gmail.com>
2022-05-18 18:37:59 +0800

    add the second solution

--- 27_remove_element.cpp
+++ 27_remove_element.cpp
@@ -27,3 +27,19 @@ public:
     return left; // // left一定指向了最终数组末尾的下一个元素
   }
 };
+
+class Solution2 {
+public:
+  int removeElement(vector<int> &nums, int val) {
+    int idx = 0; // 指向待插入位置。idx 初始值为 0
+    for (auto x : nums) {
+      // 如果当前元素 x 与移除元素 val 相同，那么跳过该元素。
+      // 否则将其放到下标 idx 的位置，并让 idx 自增右移。
+      // 返回的idx即为答案
+      if (x != val) {
+        nums[idx++] = x;
+      }
+    }
+    return idx;
+  }
+};
