43a3c377192d2d6a019818ce6515cc10c20165fb
william.Ubuntu <huo843435402@gmail.com>
2022-05-22 15:31:00 +0800

    unified programming style

--- 69_sqrt_x.cpp
+++ 69_sqrt_x.cpp
@@ -1,38 +1,26 @@
-#include <iostream>
-using namespace std;
-
 class Solution {
 public:
   int mySqrt(int x) {
     if (x == 0)
       return 0;
-    if (x == 1)
-      return 1;
 
-    int left = 1;
-    int right = x;
-
-    int res = -1;
+    int left = 1;  // 左边界
+    int right = x; // 右边界
 
     // target应该是平方后小于等于x的，如果平方后大于x
-    // target及之后的值都不符合条件【二分查找的思路】
+    // 那么target及之后右边的值都不符合条件
+    // 【二分查找】：找到第一个小于等于sqrt(x)的值
     while (left <= right) {
       int median = left + ((right - left) / 2);
-      if ((long long)median * median > x) {
-        right = median - 1;
-      } else if ((long long)median * median <= x) {
-        // 继续探究左边是不是有值平方后小于x，但更接近x
-        left = median + 1;
-        res = left;
+      if ((long long)median * median <= x) {
+        left = median + 1; // left左边的值都小于等于sqrt(x)
+      } else {
+        right = median - 1; // right右边的值都大于sqrt(x)
       }
     }
-    // 返回的res是实际答案 + 1
-    return res - 1;
+
+    // 应该返回left-1,但是循环终止时,right = left - 1
+    // 所以返回right
+    return right;
   }
 };
-
-int main() {
-  Solution *sol = new Solution();
-  cout << sol->mySqrt(8) << endl;
-  return 0;
-}
