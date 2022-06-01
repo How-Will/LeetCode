f87cf7da642066cacbabd41f6ae347bd273c1c41
william.Ubuntu <huo843435402@gmail.com>
2022-06-01 21:11:24 +0800

    ✨feat[142]: add a second solution about double pointers
    User: william

--- 142_linked_list_cycle_II.cpp
+++ 142_linked_list_cycle_II.cpp
@@ -23,3 +23,46 @@ public:
     return nullptr; // 表示链表无环
   }
 };
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution2 {
+public:
+  // 龟兔赛跑算法
+  // 假设链表结点到入环点的节点数为a,龟兔在入环点开始的
+  // 第b个结点相遇，环中剩下的结点数为c个,龟兔相遇时
+  // 兔子的路程为：a + b + n*(b+c)
+  // 乌龟的路程为：a + b
+  // 兔子的路程是乌龟的两倍：a + b + n * (b + c) = 2(a + b)
+  // -> (n - 1)(b + c) + c = a
+
+  // 龟兔相遇时路程的确定，参考代码随想录题解
+  ListNode *detectCycle(ListNode *head) {
+    ListNode *rabbit = head;
+    ListNode *turtle = head;
+    // 判断条件决定了使用do-while循环
+    do {
+      if (!rabbit || !rabbit->next)
+        return nullptr; // 说明没有环
+      rabbit = rabbit->next->next;
+      turtle = turtle->next;
+    } while (rabbit != turtle);
+
+    // 表示有环，此时rabbit和turtle在环中相遇
+    // 根据公式(n-1)(b+c)+c = a
+    // 定义一个链表头部指针，每次移动一个结点
+    // 最终它会和turtle在环入口相遇
+    ListNode *temp = head;
+    while (temp != turtle) {
+      temp = temp->next;
+      turtle = turtle->next;
+    }
+
+    return turtle; // 此时turtle和temp在入环点相遇
+  }
+};
